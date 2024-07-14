54d7089a1c286a7594c7c3348362283e67e5c51e
c1a200 <34091616+c1a200@users.noreply.github.com>
2024-07-14 14:31:25 +0800

    Update collect.py

--- subscribe/collect.py
+++ subscribe/collect.py
@@ -391,7 +391,7 @@ def aggregate(args: argparse.Namespace) -> None:
     logger.info(f"found {len(best_nodes)} proxies, save it to {list(records.values())}")
 
     life, traffic = max(0, args.life), max(0, args.flow)
-    if life > 0或traffic > 0:
+    if life > 0 or traffic > 0:
         # 过滤出新的订阅并检查剩余流量和过期时间是否满足要求
         new_subscriptions = [x for x in urls if x not in old_subscriptions]
 
